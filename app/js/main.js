7ad346db6597e13d4bdaa2aff6d3bdf7ef3e9762
yarilomonster <doc09dayz@gmail.com>
2019-11-26 20:58:58 +0300

    added header-profile, section work, template

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,12 @@
 $(function () {
 
+  $('.work__slider-inner').slick({
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="../img/icons/left.png" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="../img/icons/right.png" alt=""></button>'
+  });
 
-
+/* start mixitup */ 
+  var mixer = mixitup('.template__inner-box');
+/* end mixitup */ 
 
 });
