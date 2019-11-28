761867c590f1f2ae5ae8880111959627ab181ea9
yarilomonster <doc09dayz@gmail.com>
2019-11-28 16:12:01 +0300

    added followers, information, choose

--- app/js/main.js
+++ app/js/main.js
@@ -5,8 +5,22 @@ $(function () {
     nextArrow: '<button class="slick-arrow slick-next"><img src="../img/icons/right.png" alt=""></button>'
   });
 
+  $('.followers-slider__inner').slick({
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="../img/icons/left.png" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="../img/icons/right.png" alt=""></button>',
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  });
+
 /* start mixitup */ 
   var mixer = mixitup('.template__inner-box');
 /* end mixitup */ 
+
+  $(".rate-star").rateYo({
+    rating: 4.5,
+    starWidth: "15px",
+    readOnly: true
+  });
 
 });
