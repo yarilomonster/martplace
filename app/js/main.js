9488130e3df2d72c3ff4ad740a2f3b7b36fde02f
yarilomonster <doc09dayz@gmail.com>
2019-12-01 21:14:16 +0300

    added page new-items

--- app/js/main.js
+++ app/js/main.js
@@ -13,14 +13,36 @@ $(function () {
     slidesToScroll: 1,
   });
 
-/* start mixitup */ 
-  var mixer = mixitup('.template__inner-box');
-/* end mixitup */ 
+  $('.js-range-slider').ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 400,
+    from: 30,
+    to: 300,
+    prefix: '$'
+  });
 
   $(".rate-star").rateYo({
     rating: 4.5,
     starWidth: "15px",
     readOnly: true
   });
+
+  $(".icon-list").on('click', function() {
+    $(".template__item").addClass('list');
+    $(".icon-list").addClass('active');
+    $(".icon-th-large ").removeClass('active');
+  });
+
+  $(".icon-th-large").on('click', function() {
+    $(".template__item").removeClass('list');
+    $(".icon-th-large").addClass('active');
+    $(".icon-list").removeClass('active');
+  });
+
+/* start mixitup */ 
+  var mixer = mixitup('.template__inner-box');
+/* end mixitup */ 
+
 
 });
