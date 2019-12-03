507da52404d66a29c29a9b1d6b9bf1fe6d9b9337
yarilomonster <doc09dayz@gmail.com>
2019-12-04 00:44:00 +0300

    Fixed bugs: header, footer, work, template

--- app/js/main.js
+++ app/js/main.js
@@ -1,48 +1,17 @@
 $(function () {
 
   $('.work__slider-inner').slick({
-    prevArrow: '<button class="slick-arrow slick-prev"><img src="../img/icons/left.png" alt=""></button>',
-    nextArrow: '<button class="slick-arrow slick-next"><img src="../img/icons/right.png" alt=""></button>'
-  });
-
-  $('.followers-slider__inner').slick({
-    prevArrow: '<button class="slick-arrow slick-prev"><img src="../img/icons/left.png" alt=""></button>',
-    nextArrow: '<button class="slick-arrow slick-next"><img src="../img/icons/right.png" alt=""></button>',
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-  });
-
-  $('.js-range-slider').ionRangeSlider({
-    type: "double",
-    min: 0,
-    max: 400,
-    from: 30,
-    to: 300,
-    prefix: '$'
+    prevArrow: '<button class="slick-arrow slick-prev lnr-chevron-left"></button>',
+    nextArrow: '<button class="slick-arrow slick-next lnr-chevron-right"></button>'
   });
 
   $(".rate-star").rateYo({
     rating: 4.5,
     starWidth: "15px",
-    readOnly: true
+    readOnly: true,
+    "starSvg": "<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'><path d='M0 0h24v24H0z' fill='none'/><path d='M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z'/><path d='M0 0h24v24H0z' fill='none'/></svg>"
   });
 
-  $(".icon-list").on('click', function() {
-    $(".template__item").addClass('list');
-    $(".icon-list").addClass('active');
-    $(".icon-th-large ").removeClass('active');
-  });
-
-  $(".icon-th-large").on('click', function() {
-    $(".template__item").removeClass('list');
-    $(".icon-th-large").addClass('active');
-    $(".icon-list").removeClass('active');
-  });
-
-/* start mixitup */ 
   var mixer = mixitup('.template__inner-box');
-/* end mixitup */ 
-
 
 });
